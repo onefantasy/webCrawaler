bb6b49507a446648e8f06da1419f3ac13c42347a
onefantasy <1563478812@qq.com>
2019-11-09 10:26:03 +0800

    v1

--- v1/config.js
+++ v1/config.js
@@ -1,8 +1,8 @@
 // 数据库配置
 const sql = {
-    database: 'test', //数据库名称
+    database: '', //数据库名称
     user: 'root', //mysql用户名
-    password: 'password123', //mysql密码
+    password: '', //mysql密码
     host: '127.0.0.1', //服务器ip
     charset : 'utf8mb4'
 }
